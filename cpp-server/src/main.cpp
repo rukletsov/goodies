d1eabf11b7cb4d4fe6c56c22e7e7082af3c9349a
Alexander Rukletsov <rukletsov@gmail.com>
2020-09-13 19:27:25 +0200

    cpp-server: Clean up “main.cpp”.

--- cpp-server/src/main.cpp
+++ cpp-server/src/main.cpp
@@ -10,67 +10,12 @@
 
 int main()
 {
-//    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-//    std::string hello("Hello world!");
-//    std::cout << hello << std::endl;
-
-//    auto c = 5;
-//    std::cout << "auto int value: " << c << std::endl;
-
-//    std::shared_ptr<int> d_ptr(new int(7));
-//    std::cout << "shared_ptr value: " << *d_ptr << std::endl;
-
-//    auto e = 1 << 2;
-//    std::cout << e << std::endl;
-
-//    std::pair p(0.5, true);
-//    std::cout << p.first << std::boolalpha << " " << p.second << std::endl;
-
-//    std::optional<int> ol(10);
-//    ol.reset();
-//    std::cout << bool(ol) << std::endl;
-
-//  auto create = Socket::open_socket(8080);
-//  std::shared_ptr<Socket> socket;
-//  if (create) {
-//    socket.reset(create.value());
-//    auto s = socket->accept();
-//  }
-
-  Worker w;
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  w.try_assign([](){
-    std::cout << " > Starting work 1" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    std::cout << " > Finishing work 1" << std::endl;
-  });
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-  w.try_assign([](){
-    std::cout << " > Starting work 2" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    std::cout << " > Finishing work 2" << std::endl;
-  });
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-  w.try_assign([](){
-    std::cout << " > Starting work 3" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    std::cout << " > Finishing work 3" << std::endl;
-  });
-
-  std::cout << "Stopping" << std::endl;
-  w.shutdown();
-
-  w.try_assign([](){
-    std::cout << " > Starting work 4" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    std::cout << " > Finishing work 4" << std::endl;
-  });
-
-  w.wait();
-
+  auto create = Socket::open_socket(8080);
+  std::shared_ptr<Socket> socket;
+  if (create) {
+    socket.reset(create.value());
+    auto s = socket->accept();
+  }
 
   return 0;
 }
